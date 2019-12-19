483cf0c394cf7483c4468ffe3ef51ebaca280957
Bradley Kelman <kelmnb@elsevier.com>
2019-12-19 09:35:41 +0000

    added code structure for day 2

---
+++ Day2/Part1.py
@@ -0,0 +1,17 @@
+# Read in the text file
+
+def opcide():
+   #decides when to use add or multiply function based on numbers presented
+
+def add_opcode():
+   #adds the numbers presented
+
+def multiply_opcode():
+   #Now multiplies number presented
+
+
+def main(inputtext):
+   # input of the text file
+   # call function and move forward in string of numbers by 4 and repeat
+   # if number is 99 then break / return
+

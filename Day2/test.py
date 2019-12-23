f721472755b57f9859e2d5557b6b4f53edcc1c46
Bradley Kelman <kelmnb@elsevier.com>
2019-12-23 11:58:22 +0000

    day2 part one completed

---
+++ Day2/test.py
@@ -0,0 +1,31 @@
+import numpy as np
+data = open("Day2Input.txt", "r")
+inputs = data.readlines()
+
+def string_to_array(inputs):
+
+    # print(inputs)
+    map(int, inputs.split(','))
+    # array2 = np.asarray(inputs)
+    for i in inputs:
+        newinput = inputs.append(int(inputs[i]))
+        newarray = np.asarray(newinput)
+    return newarray
+
+print(string_to_array(inputs))
+# print()
+# print(array2)
+inputarray1 = [1, 0, 0, 3, 1, 1, 2, 3, 1, 3, 4, 3, 1, 5, 0, 3, 2, 10, 1, 19, 2, 9, 19, 23, 2, 13, 23, 27, 1, 6, 27, 31,
+              2, 6, 31, 35, 2, 13, 35, 39, 1, 39, 10, 43, 2, 43, 13, 47, 1, 9, 47, 51, 1, 51, 13, 55, 1, 55, 13, 59, 2,
+              59, 13, 63, 1, 63, 6, 67, 2, 6, 67, 71, 1, 5, 71, 75, 2, 6, 75, 79, 1, 5, 79, 83, 2, 83, 6, 87, 1, 5, 87,
+              91, 1, 6, 91, 95, 2, 95, 6, 99, 1, 5, 99, 103, 1, 6, 103, 107, 1, 107, 2, 111, 1, 111, 5, 0, 99, 2, 14, 0,
+              0]
+
+# print(inputarray1)
+# print(inputarray1[10])
+# def converter(test_list):
+#     # res = ("".join(map(int, inputs)))
+#     test_list = list(map(int, test_list))
+#     return test_list
+#
+# print(converter(inputs))
